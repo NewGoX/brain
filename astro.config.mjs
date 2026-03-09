f5a7507dd2e0d95c0c51b1401bc3ef61076c5753
黄德福 <hdf@192.168.10.12>
2026-03-10 03:58:45 +0800

    配置侧边栏自动生成

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,18 +9,25 @@ export default defineConfig({
 			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+                {
+                    label: '开始使用', // 把原本的 Guides 改成中文
+                    items: [
+                        { label: '示例指南', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Web3 安全篇', // 你新的一级目录
+                    autogenerate: { directory: 'beidaoleixing' }, // 自动扫描 web3-security 文件夹
+                },
+                {
+                    label: 'Web3 出入金', // 另一个一级目录
+                    autogenerate: { directory: 'web3_churujin' }, // 自动扫描 web3-ramp 文件夹
+                },
+                {
+                    label: '参考资料', // 把原本的 Reference 改成中文
+                    autogenerate: { directory: 'reference' },
+                },
+            ],
 		}),
 	],
 });
